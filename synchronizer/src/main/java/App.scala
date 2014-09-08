e8d31ffe2703c6a005607d28d12a8c6621b28c15
Zbyszko <mephiss@gmail.com>
2014-09-08 19:21:13 +0200

    initial commit

---
+++ synchronizer/src/main/java/App.scala
@@ -0,0 +1,15 @@
+/**
+ * Created by mephi_000 on 2014-09-08.
+ */
+
+import com.twitter.finagle.{Http, Service}
+import com.twitter.util.{Await, Future}
+import java.net.InetSocketAddress
+import org.jboss.netty.handler.codec.http._
+
+
+object testComicVine extends App {
+  
+  
+
+}
